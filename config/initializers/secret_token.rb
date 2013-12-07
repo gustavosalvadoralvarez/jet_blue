8253b3f60b2655fe76111de4cd37d15b88b53675
gustavosalvadoralvarez <gustavosalvadoralvarez@gmail.com>
2013-12-06 20:27:40 -0500

    Fixed security token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,16 @@
-# Be sure to restart your server when you modify this file.
+require 'securerandom'
 
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
 
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-JetBlue::Application.config.secret_key_base = 'd24ab90e90c88ebdf803ca8cc93f03934bac7f9e20f9151106dbf015d7e3c72e36ef77ac9f0028208bf6a53da4edea92b5e5d766786e4bbdb56825bca868b9d0'
+SampleApp::Application.config.secret_key_base = secure_token
