db7ea0eb30d1ce07cce875227fc3f96fbf240e30
gustavosalvadoralvarez <gustavosalvadoralvarez@gmail.com>
2013-12-06 20:23:22 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JetBlue::Application.config.secret_key_base = 'd24ab90e90c88ebdf803ca8cc93f03934bac7f9e20f9151106dbf015d7e3c72e36ef77ac9f0028208bf6a53da4edea92b5e5d766786e4bbdb56825bca868b9d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JetBlue::Application.config.session_store :cookie_store, key: '_jet_blue_session'
