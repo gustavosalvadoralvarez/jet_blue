9e6833e7f010cdd732eb10c70d7aa696b8dfcbc4
gustavosalvadoralvarez <gustavosalvadoralvarez@gmail.com>
2013-12-06 21:58:36 -0500

    Begin styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 JetBlue::Application.routes.draw do
+  get "static_pages/login"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
